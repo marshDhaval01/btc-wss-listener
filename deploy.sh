2131a1141ffb0c1febc5fef24685fa86a9c0a711
marshDhaval01 <Dhaval@marsmatics.com>
2025-04-17 10:57:10 -0400

    Docker Testing

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
+cd ~/btc-wss-listener
+git pull origin main
+docker stop btc-wss-listener
+docker rm btc-wss-listener
+docker build -t btc-wss-listener .
+docker run -d -p 3000:3000 --name btc-listener --restart unless-stopped btc-wss-listener
 
-echo "📥 Pulling latest image from Docker Hub..."
-docker pull marsdevd/btc-wss-listener:latest
-
-echo "🛑 Stopping and removing old container..."
-docker stop btc-wss-listener 2>/dev/null
-docker rm btc-wss-listener 2>/dev/null
-
-echo "🚀 Running updated container..."
-docker run -d -p 3000:3000 --name btc-wss-listener --restart unless-stopped marsdevd/btc-wss-listener:latest
-
-echo "✅ Deployment complete. Container is running on port 3000."
